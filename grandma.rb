4584d973817c1afa21a16492823493d10117a922
Alyssa Yates <bosslys@icloud.com>
2019-09-22 18:31:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 message = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(message)
-    message puts "I LOVE YOU TOO PUMPKIN!"
+  if message == uppercase
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end 
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
